e497b116aac82c79b21239b75a568a1f69ee042e
Jacob Grisham <jacob.d.grisham@gmail.com>
2020-07-28 14:54:11 -0700

    Add functionality to app.js and create directories to organize files

--- app.js
+++ app.js
@@ -1,20 +1,13 @@
 // Calling dependencies in package.json
 const express 				= require("express"),
-			app 						= express(),
-			bodyParser 			= require("body-parser"),
-			flash						= require("connect-flash"),
-  		methodOverride	= require("method-override");
+			app 						= express();
 
 // Setting up Express
-app.use(bodyParser.urlencoded({extended: true}));
-app.use(express.static(__dirname + "/public")); // Using __dirname + is a better way to navigate to the file
-app.set("view engine", "ejs");
-app.use(methodOverride("_method"));
-app.use(flash());
+app.use(express.static("public"));
 
 
 // Setting the port for the server
-const port = process.env.PORT || 3000;
-app.listen(port, process.env.IP, function(){
+const port = 3000;
+app.listen(port, () => {
 	console.log("Server is on")
 });
