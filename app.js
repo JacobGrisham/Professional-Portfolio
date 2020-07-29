13663d3fa0b4a037fb331db51d63a1008b46adb7
Jacob Grisham <jacob.d.grisham@gmail.com>
2020-07-29 11:45:13 -0700

    Add npm package heroku-ssl-redirect and update app.js. Update index.html

--- app.js
+++ app.js
@@ -1,11 +1,15 @@
 // Calling dependencies in package.json
-const express 				= require("express"),
+const sslRedirect 		= require('heroku-ssl-redirect'),
+			express 				= require("express"),
 			app 						= express();
+
+// Enable ssl redirect
+app.use(sslRedirect());
 
 // Setting up Express
 app.use(express.static("public"));
 
-// use port 3000 unless there exists a preconfigured port
+// Use port 3000 unless there exists a preconfigured port
 var port = process.env.PORT || 3000;
 app.listen(port, process.env.IP, () => {
 	console.log("Server is on");
