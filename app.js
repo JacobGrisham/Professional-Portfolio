97c9990e491fbce571692487229bc2c67c8d569e
Jacob Grisham <jacob.d.grisham@gmail.com>
2020-07-31 12:47:01 -0700

    Add smaller images, add meta tags, adjust project card sub-heading, and adjust bullet points on methodology

--- app.js
+++ app.js
@@ -9,6 +9,13 @@ app.use(sslRedirect());
 // Setting up Express
 app.use(express.static("public"));
 
+//const publicPath = express.static(join(__dirname, "../public"));
+//app.use("public", publicPath);
+
+app.get("/thumbnail", function(req,res){        
+	res.send('<img src="/public/img/thumbnail.png" />');
+});
+
 // Use port 3000 unless there exists a preconfigured port
 var port = process.env.PORT || 3000;
 app.listen(port, process.env.IP, () => {
