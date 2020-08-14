f10ebd6f9d9690d1c004ab7cd9d0d35af79aad34
Jacob Grisham <jacob.d.grisham@gmail.com>
2020-08-13 22:42:47 -0700

    Add loading screen, add media query for skill boxes to slide up

--- public/index.js
+++ public/index.js
@@ -1,3 +1,14 @@
+// Loading screen
+document.onreadystatechange = () => { 
+  if (document.readyState !== "complete") { 
+      document.querySelector("body").style.visibility = "hidden"; 
+      document.querySelector("#loader").style.visibility = "visible"; 
+  } else { 
+      document.querySelector("#loader").style.display = "none"; 
+      document.querySelector("body").style.visibility = "visible"; 
+  } 
+}; 
+
 // Parallax with lax.js
 window.onload = function() {
 	lax.setup() // init
