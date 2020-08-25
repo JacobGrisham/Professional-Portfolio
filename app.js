61bc72d10ed32e3a2689d4996ead547b0ceb3c74
Jacob Grisham <jacob.d.grisham@gmail.com>
2020-08-25 00:33:56 -0700

    Add content security policy

--- app.js
+++ app.js
@@ -12,6 +12,20 @@ app.set('view engine', 'ejs');
 app.use(compression()); // Compress Javascript
 
 app.use(helmet()); // Helmet helps you secure your Express apps by setting various HTTP headers.
+app.use(
+  helmet.contentSecurityPolicy({
+    directives: {
+      defaultSrc: ["'none'", "http://localhost:3000/img/sprite.svg", "https://www.jacobgrisham.com/img/sprite.svg"],
+      scriptSrc: ["'self'", "https://cdn.jsdelivr.net/npm/lax.js", "https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js", "https://connect.facebook.net/en_US/sdk.js"],
+			objectSrc: ["'none'"],
+			fontSrc: ["https://fonts.gstatic.com"],
+			imgSrc: ["'self'", "https://www.facebook.com/tr/?id=2733409333567092&ev=fb_page_view&dl=http%3A%2F%2Flocalhost%3A3000%2F&rl=&if=false&ts=1598340316655&sw=1440&sh=900&at="],
+			styleSrc: ["'self'", "https://fonts.googleapis.com", "'sha256-tsYFq5pUcggQKFXnvmlvUrk8MgTJLL1Gjnqenv201b8='"],
+			manifestSrc: ["https://www.jacobgrisham.com/site.webmanifest", "http://localhost:3000/site.webmanifest"],
+			frameSrc: ["http://www.youtube.com/"]
+    },
+  })
+);
 
 app.get("/", function(req,res){
 	res.render("index");
