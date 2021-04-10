6c92713fbb27411fc7dfeab6323505ec069ffa4f
Jacob Grisham <jacob.d.grisham@gmail.com>
2021-04-10 09:55:32 -0700

    remove facebook analytics because the service is being discontinued

--- public/js/index.js
+++ public/js/index.js
@@ -139,24 +139,3 @@ var date = new Date();
 var year = date.getFullYear();
 
 document.getElementById("date").innerHTML = year;
-
-// Facebook Analytics
-  window.fbAsyncInit = function() {
-    FB.init({
-      appId      : '2733409333567092',
-      cookie     : true,
-      xfbml      : true,
-      version    : 'v8.0'
-    });
-      
-    FB.AppEvents.logPageView();   
-      
-  };
-
-  (function(d, s, id){
-     var js, fjs = d.getElementsByTagName(s)[0];
-     if (d.getElementById(id)) {return;}
-     js = d.createElement(s); js.id = id;
-     js.src = "https://connect.facebook.net/en_US/sdk.js";
-     fjs.parentNode.insertBefore(js, fjs);
-   }(document, 'script', 'facebook-jssdk'));
